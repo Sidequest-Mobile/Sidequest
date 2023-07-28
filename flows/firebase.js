1dc5c28eb9c4db94e3c029ee3cae44d7e1a08563
Benji Cardon <Benjaminlibrarian@gmail.com>
2023-07-28 01:58:30 -0600

    Changed Export Pattern

--- flows/firebase.js
+++ flows/firebase.js
@@ -3,6 +3,4 @@ import app from '../firebaseConfig.js';
 var auth = app.auth()
 var database = app.database();
 
-module.exports.auth = auth;
-module.exports.database = database;
-
+export default { auth, database };
