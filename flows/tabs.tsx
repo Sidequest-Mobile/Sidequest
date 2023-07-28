75abafe6352725df79490116f2f01b90151f0e12
Noah Beito <noah.beito@gmail.com>
2023-07-27 23:14:01 -0700

    updated the imported component names to match the exported function names in tabs.tsx

--- flows/tabs.tsx
+++ flows/tabs.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import FeedScreen from './community/feed';
-import ProfileScreen from './account/profile';
+import Feed from './community/feed';
+import Profile from './account/profile';
 
 const Tab = createMaterialBottomTabNavigator();
 
 export default function Tabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Feed" component={FeedScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen name="Feed" component={Feed} />
+      <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
   );
 }
