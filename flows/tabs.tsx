bbfc6e11633e1e4d8077e92d6e669ae6576da12a
Noah Beito <noah.beito@gmail.com>
2023-07-31 21:17:42 -0700

    added search tab

--- flows/tabs.tsx
+++ flows/tabs.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import Feed from './community/feed';
 import Profile from './account/profile';
+import Search from './consume/search';
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -9,6 +10,7 @@ export default function Tabs() {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Feed" component={Feed} />
+      <Tab.Screen name="Search" component={Search} />
       <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
   );
