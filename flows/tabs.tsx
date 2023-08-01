5017f2f78d18ce23d16928d14dab4f87d5218cdb
Noah Beito <noah.beito@gmail.com>
2023-08-01 00:11:24 -0700

    installed material community items and added home icon to bottom tab nav bar

--- flows/tabs.tsx
+++ flows/tabs.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Feed from './community/feed';
 import Profile from './account/profile';
 import Search from './consume/search';
@@ -8,8 +9,21 @@ const Tab = createMaterialBottomTabNavigator();
 
 export default function Tabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Feed" component={Feed} />
+    <Tab.Navigator
+      initialRouteName='Feed'
+      activeColor='#e91e63'
+      barStyle={{ backgroundColor: 'tomato' }}
+    >
+      <Tab.Screen
+        name="Feed"
+        component={Feed}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
       <Tab.Screen name="Search" component={Search} />
       <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
