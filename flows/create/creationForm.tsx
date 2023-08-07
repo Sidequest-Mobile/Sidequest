f5926378cc889ec3bd8caed61f753038a02fc83f
Benji Cardon <Benjaminlibrarian@gmail.com>
2023-08-07 01:37:12 -0600

    Added eslint and prettier configs, as well main form state for quest creation

--- flows/create/creationForm.tsx
+++ flows/create/creationForm.tsx
@@ -1,139 +1,182 @@
-import React from 'react';
-import { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, Pressable, } from 'react-native';
-import  firebase  from '../firebase.js';
-import { collection } from 'firebase/firestore'
+import { collection } from 'firebase/firestore';
+import React, { useState } from 'react';
+import {
+  Button,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+import firebase from '../firebase.js';
 
-let quests = collection( firebase.firestore, "/quests")
-let UID : string = "0";
-// just a dummy as a placeholder for now
-// What problems do we need to solve with this component?
-// Stated updates to the data
-// tags
-
-// Location - getting
-// Media upload- picture
-// Sharing AUth info with firestore database
+let quests = collection(firebase.firestore, '/quests');
+let UID: string = '0';
 
 export default function CreateQuest() {
   // General States
   const [type, setType] = useState('undecided');
   const [tagline, setTagline] = useState('');
   const [description, setDescription] = useState('');
-  let tagsInit: Array<string> = []
+  let tagsInit: Array<string> = [];
   const [tags, setTags] = useState(tagsInit);
   const [tagText, setTagText] = useState('');
   // Quiz States
   const [quizQuestion, setQuizQuestion] = useState('');
   const [quizCorrectAnswer, setQuizCorrectAnswer] = useState('');
-  let quiz: Array<string> = []
+  let quiz: Array<string> = [];
+  const [quizIncorrectAnswer, setQuizIncorrectAnswer] = useState('');
   const [quizIncorrectAnswers, setQuizIncorrectAnswers] = useState(quiz);
   // Media States
   const [satisfyingCondition, setSatisfyingCondition] = useState('');
-  const [exampleURL, setExampleURL] = useState('')
+  const [exampleURL, setExampleURL] = useState('');
   // Location States
-  const [assignedLocation, setAssignedLocation] = useState('')
+  const [assignedLocation, setAssignedLocation] = useState('');
 
-  // add state that follows each of our TextInputs
-  // This would be - Tagline, Description, Quiz and answers, Satisfying Conditions
   // each pressable should adjust the "type" state to match it's internal text value. This should also correlate to the conditional display logic.
   // Changing type should also reset the states for the unchosen types
   // When that type is chosen, the pressable should have a change to the style of the view to show that it is the chosen type.
+  function changeType(newType: string): void {
+    setType(newType);
+    setQuizQuestion('');
+    setQuizCorrectAnswer('');
+    setQuizIncorrectAnswer('');
+    setQuizIncorrectAnswers(quiz);
+    setSatisfyingCondition('');
+    setExampleURL('');
+    setAssignedLocation('');
+  }
   // Choosing location should have a reference to the native Geolocation API.
   // There should be a way for us to use the native file API to open up an image browser/camera which users can choose a photo/video from
   // we'll need a way to "upload" that file, then store a URL as a state for the optional picture.
   // When we press "submit" there should be some data validation- then if the thing passes then we'll make a document with the published property
   // When we press "save" we will make a document/ update the document with the properties, but published as false.
-  let addTag = function(): void {
-    setTags([...tags, tagText])
+  let addTag = function (): void {
+    setTags([...tags, tagText]);
+    setTagText('');
+  };
 
-
-  }
-
+  let addAnswer = function (): void {
+    setQuizIncorrectAnswers([...quizIncorrectAnswers, quizIncorrectAnswer]);
+    setQuizIncorrectAnswer('');
+  };
 
   return (
     <View style={styles.container}>
       <Text>Create Quest</Text>
       <Text>Tagline</Text>
       <TextInput
-      onChangeText={setTagline}
-      value={tagline}
-      placeholder='This Quest in one sentence'/>
+        onChangeText={setTagline}
+        value={tagline}
+        placeholder="This Quest in one sentence"
+      />
 
       <Text>Description</Text>
       <TextInput
-      onChangeText={setDescription}
-      value={description}
-      placeholder="Type a description of your quest here"
+        onChangeText={setDescription}
+        value={description}
+        placeholder="Type a description of your quest here"
       />
 
       <Text>Picture, optional</Text>
       <Text>Quest Type</Text>
 
-      <Pressable>
-        <View style={{
-          backgroundColor:"blue",
-          borderRadius:10,
-          height:20,
-        }}>
-        <Text>Location</Text>
+      <Pressable onPress={() => changeType('location')}>
+        <View
+          style={{
+            backgroundColor: 'blue',
+            borderRadius: 10,
+            height: 20,
+          }}>
+          <Text>Location</Text>
         </View>
       </Pressable>
 
-      <Pressable>
-      <View style={{
-          backgroundColor:"yellow",
-          borderRadius:10,
-          height:20,
-        }}>
-        <Text> Media </Text>
+      <Pressable onPress={() => changeType('media')}>
+        <View
+          style={{
+            backgroundColor: 'yellow',
+            borderRadius: 10,
+            height: 20,
+          }}>
+          <Text> Media </Text>
         </View>
       </Pressable>
 
-      <Pressable>
-      <View style={{
-          backgroundColor:"red",
-          borderRadius:10,
-          height:20,
-        }}>
-        <Text> Quiz </Text>
+      <Pressable onPress={() => changeType('quiz')}>
+        <View
+          style={{
+            backgroundColor: 'red',
+            borderRadius: 10,
+            height: 20,
+          }}>
+          <Text> Quiz </Text>
         </View>
       </Pressable>
 
       <Text>Tags</Text>
-
+      {tags.map(
+        (val): JSX.Element => (
+          <Text>{val}</Text>
+        ),
+      )}
       <TextInput
-      onChangeText={setTagText}
-      value={tagText}
-      placeholder='Write a tag for your quest to be identified by'/>
-      <Button title='Add Tag' />
-
+        onChangeText={setTagText}
+        value={tagText}
+        placeholder="Write a tag for your quest to be identified by"
+      />
+      <Button title="Add Tag" onPress={addTag} />
+      {}
       {type === 'quiz' && (
-      <>
-      <Text>Quiz Question </Text>
-      <TextInput/>
-      <Text>Quiz Answers</Text>
-      <TextInput/>
-      <TextInput/>
-      <TextInput/>
-      <TextInput/>
-      </>)}
-
+        <>
+          <Text>Quiz Question </Text>
+          <TextInput
+            onChangeText={setQuizQuestion}
+            value={quizQuestion}
+            placeholder="What question will the quester answer?"
+          />
+          <Text>The Correct Answer</Text>
+          <TextInput
+            onChangeText={setQuizCorrectAnswer}
+            value={quizCorrectAnswer}
+            placeholder="What is the correct answer to the quiz?"
+          />
+          <Text>Other Answers</Text>
+          <TextInput
+            onChangeText={setQuizIncorrectAnswer}
+            value={quizIncorrectAnswer}
+            placeholder="Add an incorrect Answer"
+          />
+          <Button title="Add Answer" onPress={addAnswer} />
+        </>
+      )}
+      {quizIncorrectAnswers.map(
+        (val): JSX.Element => (
+          <Text>{val}</Text>
+        ),
+      )}
       {type === 'location' && (
-      <>
-        <Text>Location</Text>
-      </>)}
+        <>
+          <Text>Location</Text>
+        </>
+      )}
 
-      {type === 'media' && (<>
-      <Text>Satisfying Condition</Text>
-      <TextInput/>
-      <Text>Example Media</Text>
+      {type === 'media' && (
+        <>
+          <Text>Satisfying Condition</Text>
+          <TextInput
+            placeholder="How can one tell if the quest has been completed?"
+            value={satisfyingCondition}
+            onChangeText={setSatisfyingCondition}
+          />
 
-      </>)}
-      <Button title='Save'/>
-      <Button title='Post'/>
+          <Text>Example Media</Text>
+        </>
+      )}
+      <Button title="Save" />
+      <Button title="Post" />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -144,4 +187,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
