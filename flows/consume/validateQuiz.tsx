73e89b476f2dba776a2b9d0737bbce4a932fb715
Benji Cardon <Benjaminlibrarian@gmail.com>
2023-09-05 12:29:04 -0600

    Some small changes

--- flows/consume/validateQuiz.tsx
+++ flows/consume/validateQuiz.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import { collection, doc, getDoc } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
 import { Button, Pressable, Text, View } from 'react-native';
+import firebase from '../firebase';
 
-function ValidateQuiz({ navigation }) {
+const questConnect = collection(firebase.firestore, '/quests');
+
+function ValidateQuiz({ navigation, route }) {
+  const [loaded, setLoaded] = useState(false);
+  const [wrongAnswers, setWrongAnswers] = useState(['']);
+  const [rightAnswer, setRightAnswer] = useState('');
+  const [question, setQuestion] = useState('');
+  const [selected, setSelected] = useState('');
+
+  useEffect(() => {
+    getQuest().then(quest => {
+      setWrongAnswers(quest.incorrect_answers);
+      setQuestion(quest.question);
+      setRightAnswer(quest.answer);
+      setLoaded(true);
+    });
+  }, []);
+
+  async function getQuest() {
+    const docref = doc(questConnect, `${route.params.id}`);
+    const quest = await getDoc(docref);
+    return quest.data();
+  }
+
   return (
     <View>
-      <Pressable>
-        <Text>Go back to Quest</Text>
-      </Pressable>
       <Text>List of Quests in progress as tiles</Text>
+
       <Button
         title="Go to Rate"
         onPress={e => navigation.navigate('Rate')}></Button>
