d3241883c4e51e57591bc2076038e85e821dabd7
Benji Cardon <Benjaminlibrarian@gmail.com>
2023-10-04 10:55:56 -0600

    Added some functionality to the "Quest" component. Join quest now makes a quest instance-

--- flows/consume/search.tsx
+++ flows/consume/search.tsx
@@ -1,7 +1,5 @@
-import { Entypo, Feather } from '@expo/vector-icons';
 import React, { useState } from 'react';
-import { Button, Keyboard, StyleSheet, TextInput, View } from 'react-native';
-import SearchTypeDropdown from './searchtype';
+import { StyleSheet, View } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -42,63 +40,15 @@ type SearchProps = {
   setClicked: boolean;
 };
 
-const SearchBar = ({ clicked, searchPhrase, setSearchPhrase, setClicked, navigation }: SearchProps) => {
-
+const SearchBar = ({
+  clicked,
+  searchPhrase,
+  setSearchPhrase,
+  setClicked,
+  navigation,
+}: SearchProps) => {
   const [selected, setSelected] = useState<string>('all');
 
-
-  return (
-    <View style={styles.container}>
-      <SearchTypeDropdown />
-      <Button
-        title="Placeholder for Quest"
-        onPress={e => navigation.navigate('Quest')}></Button>
-      <View
-        style={
-          clicked ? styles.searchBar__clicked : styles.searchBar__unclicked
-        }>
-        {/* search Icon */}
-        <Feather
-          name="search"
-          size={20}
-          color="black"
-          style={{ marginLeft: 1 }}
-        />
-        {/* Input field */}
-        <TextInput
-          style={styles.input}
-          placeholder="Search"
-          value={searchPhrase}
-          onChangeText={setSearchPhrase}
-          onFocus={() => {
-            setClicked(true);
-          }}
-        />
-        {/* cross Icon, depending on whether the search bar is clicked or not */}
-        {clicked && (
-          <Entypo
-            name="cross"
-            size={20}
-            color="black"
-            style={{ padding: 1 }}
-            onPress={() => {
-              setSearchPhrase('');
-            }}
-          />
-        )}
-      </View>
-      {/* cancel button, depending on whether the search bar is clicked or not */}
-      {clicked && (
-        <View>
-          <Button
-            title="Cancel"
-            onPress={() => {
-              Keyboard.dismiss();
-              setClicked(false);
-            }}></Button>
-        </View>
-      )}
-    </View>
-  );
+  return <View style={styles.container}></View>;
 };
 export default SearchBar;
