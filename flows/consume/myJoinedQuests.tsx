401f31b415bd6bb084ef05b8fcc0a8370290340e
Benji Cardon <Benjaminlibrarian@gmail.com>
2023-10-24 16:59:14 -0600

    Added joined quests and most of search

--- flows/consume/myJoinedQuests.tsx
+++ flows/consume/myJoinedQuests.tsx
@@ -1,13 +1,124 @@
-import React from 'react';
-import { Button, Pressable, Text, View } from 'react-native';
+import {
+  DocumentData,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+} from 'firebase/firestore';
+import React, { useContext, useEffect, useState } from 'react';
+import { Button, Pressable, ScrollView, Text, View } from 'react-native';
+import { appContext } from '../../App';
+import QuestCard from '../components/questCard';
+import firebase from '../firebase';
+const questCollection = collection(firebase.firestore, '/quests');
+const questInstanceCollection = collection(
+  firebase.firestore,
+  '/quest_instance',
+);
+type empty = true | false;
 
 function MyJoinedQuests({ navigation }) {
+  const app = useContext(appContext);
+  const [joinedQuestInstances, setJoinedQuestInstances] = useState(
+    [] as Array<DocumentData>,
+  );
+  const [questinfo, setQuestInfo] = useState([] as Array<DocumentData>);
+  const [empty, setEmpty] = useState(true);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    getQuests();
+  }, []);
+  async function getQuests() {
+    const quests_in_progress_query = query(
+      questInstanceCollection,
+      where('userID', '==', app.userID),
+      where('completion_status', '==', 'In Progress'), // need to add type to context in App.tsx
+    );
+    getDocs(quests_in_progress_query).then(results => {
+      if (results.size) {
+        const questInst = results.docs.map(document => {
+          return document.data();
+        });
+        getQuestInfos(questInst);
+      } else {
+        setEmpty(true);
+        setLoaded(true);
+      }
+    });
+  }
+  function getQuestInfos(questinst) {
+    const queryPromises = questinst.map(inst => {
+      return getDoc(doc(firebase.firestore, '/quests/' + inst.questID));
+    });
+    Promise.all(queryPromises).then(questinfos => {
+      setEmpty(false);
+      setLoaded(true);
+      setQuestInfo(
+        questinfos.map(questinfo => {
+          return { ...questinfo.data(), id: questinfo.id };
+        }),
+      );
+      setJoinedQuestInstances(questinst);
+    });
+  }
+
   return (
     <View>
       <Pressable>
         <Text>Go back to "Profile!</Text>
       </Pressable>
       <Text>List of Quests in progress as tiles</Text>
+
+      {loaded && !empty && (
+        <ScrollView>
+          {joinedQuestInstances.map(questInst => {
+            const info = questinfo.find(
+              quest => quest.id === questInst.questID,
+            );
+            let nav = function () {
+              navigation.navigate('Quest', { id: questInst.questID });
+            };
+            return (
+              <>
+                <QuestCard
+                  name={questInst.questID}
+                  description={
+                    info !== undefined ? info.description : 'no Description'
+                  }
+                  location={
+                    info !== undefined
+                      ? 'Latitude' + info.lat + 'Longitude' + info.lng
+                      : 'no Description'
+                  }
+                  picture={
+                    info !== undefined &&
+                    info.quest_image_location !== undefined
+                      ? info.quest_image_location
+                      : 'questMain_userID_iulgm135F0R0g84PcLfUQF5dSkz2_QuestNumber_1'
+                  }
+                  onPress={nav}
+                />
+              </>
+            );
+          })}
+        </ScrollView>
+      )}
+
+      {loaded && empty && (
+        <View>
+          <Text>It seems you haven't joined any quests yet.</Text>
+        </View>
+      )}
+
+      {loaded || (
+        <View>
+          <Text>It seem the data hasn't loaded yet.</Text>
+        </View>
+      )}
+
       <Button
         title="Go to individual quest"
         onPress={e => navigation.navigate('Quest')}></Button>
--- flows/consume/search.tsx
+++ flows/consume/search.tsx
@@ -1,106 +1,229 @@
-import { getDownloadURL, getStorage, ref } from 'firebase/storage';
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import QuestCard from '../components/questCard';
+import { Picker } from '@react-native-picker/picker';
+import {
+  QueryFieldFilterConstraint,
+  collection,
+  getDocs,
+  query,
+  where,
+} from 'firebase/firestore';
+import React, { useState } from 'react';
+import { Button, Pressable, Text, TextInput, View } from 'react-native';
+import firebase from '../firebase';
+const questCollection = collection(firebase.firestore, '/quests');
 
-const styles = StyleSheet.create({
-  container: {
-    margin: 15,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    flexDirection: 'row',
-    width: '90%',
-  },
-  searchBar__unclicked: {
-    padding: 10,
-    flexDirection: 'row',
-    width: '95%',
-    backgroundColor: '#d9dbda',
-    borderRadius: 20,
-    alignItems: 'center',
-  },
-  searchBar__clicked: {
-    padding: 10,
-    flexDirection: 'row',
-    width: '80%',
-    backgroundColor: '#d9dbda',
-    borderRadius: 20,
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-  },
-  input: {
-    fontSize: 20,
-    marginLeft: 10,
-    width: '90%',
-  },
-});
-
-type SearchProps = {
-  clicked: boolean;
-  searchPhrase: string;
-  setSearchPhrase: string;
-  setClicked: boolean;
+type filters = {
+  tags: Array<QueryFieldFilterConstraint>;
+  distance?: QueryFieldFilterConstraint;
+  type: Array<QueryFieldFilterConstraint>;
 };
+const Search = ({}) => {
+  const [wheres, setWheres] = useState({
+    type: [],
+    tags: [],
+  } as filters);
 
-const SearchBar = ({
-  clicked,
-  searchPhrase,
-  setSearchPhrase,
-  setClicked,
-  navigation,
-}: SearchProps) => {
-  const [selected, setSelected] = useState<string>('all');
+  const [mode, setMode] = useState('empty');
+  const [input, setInput] = useState('');
+  const [filterDisplay, setFilterDisplay] = useState({
+    tags: [],
+    type: [],
+    minimumAverageRating: 0,
+    targetDifficulty: 0,
+    textIncludes: [],
+    maxDistance: 0,
+  });
 
-  // ********************************************************************** //
-  // CODE BELOW USED TO TEST QUEST CARD COMPONENT //
-  const [uri, setUri] = useState<string | null>(null);
+  const [quests, setQuests] = useState({});
 
-  // Get and create a reference to Firebase Storage
-  const storage = getStorage();
-  const profilePicRef = ref(storage, `profile.jpg`);
+  function addFilter() {
+    let newWhere: QueryFieldFilterConstraint;
+    switch (mode) {
+      case 'Tags':
+        newWhere = where('tags', 'array-contains', input);
+        setWheres({ type: wheres.type, tags: [...wheres.tags, newWhere] });
+        setFilterDisplay({
+          ...filterDisplay,
+          tags: [...filterDisplay.tags, input],
+        });
+        break;
+      case 'Type':
+        newWhere = where('type', '==', input);
+        setWheres({ type: [...wheres.type, newWhere], tags: [...wheres.tags] });
+        setFilterDisplay({
+          ...filterDisplay,
+          type: [...filterDisplay.type, input],
+        });
+        break;
+      case 'Distance':
+        setFilterDisplay({ ...filterDisplay, maxDistance: Number(input) });
+        break;
+      case 'Difficulty':
+        setFilterDisplay({ ...filterDisplay, targetDifficulty: Number(input) });
+        break;
+      case 'Rating':
+        setFilterDisplay({
+          ...filterDisplay,
+          minimumAverageRating: Number(input),
+        });
+        break;
+      case 'Text':
+        setFilterDisplay({
+          ...filterDisplay,
+          textIncludes: [...filterDisplay.textIncludes, input],
+        });
+        break;
+    }
+    setInput('');
+    setMode('empty');
+  }
 
-  useEffect(() => {
-    getDownloadURL(profilePicRef)
-      .then(uri => {
-        setUri(uri);
-      })
-      .catch(error => {
-        switch (error.code) {
-          case 'storage/object-not-found':
-            // File doesn't exist
-            break;
-          case 'storage/unauthorized':
-            // User doesn't have permission to access the object
-            break;
-          case 'storage/canceled':
-            // User canceled the upload
-            break;
-          case 'storage/unknown':
-            // Unknown error occurred, inspect the server response
-            break;
-          case 'storage/quota-exceeded':
-            // Free storage quota exceeded
-            break;
-          case 'storage/unauthenticated':
-            // User is unauthenticated
-            break;
+  function changeMode(itemValue, itemIndex) {
+    setMode(itemValue);
+    setInput('');
+    console.log(filterDisplay);
+  }
+
+  function runQuery() {
+    if (filterDisplay.maxDistance === 0) {
+      const q = query(questCollection, ...wheres.type, ...wheres.tags);
+      getDocs(q).then(docs => {
+        if (docs.size) {
+          if (filterDisplay.targetDifficulty) {
+            docs.docs.filter(doc => {
+              let difficulty = [...doc.data.difficulty_rating].slice(0, 5);
+            });
+          }
+          if (filterDisplay.textIncludes.length) {
+          }
+          if (filterDisplay.minimumAverageRating) {
+          }
         }
       });
-  }, []);
-
-  // ***************************************************************************** //
+    }
+  }
 
   return (
-    <>
-      <View style={styles.container}></View>
-      <QuestCard
-        name={'Quest 1'}
-        description={'this is a quest description'}
-        location={'Chiswick'}
-        picture={{ uri }}
-        onPress={() => {}}
-      />
-    </>
+    <View>
+      <Picker selectedValue={mode} onValueChange={changeMode}>
+        <Picker.Item label="Tags" value="Tags" />
+        <Picker.Item label="Type" value="Type" />
+        <Picker.Item label="Distance" value="Distance" />
+        <Picker.Item label="Difficulty" value="Difficulty" />
+        <Picker.Item label="Rating" value="Rating" />
+        <Picker.Item label="Text" value="Text" />
+      </Picker>
+      <View>
+        <Text>Where we'll display the kind of input that's possible. </Text>
+        {mode === 'Tags' && (
+          <View>
+            <Text>Tags</Text>
+            <TextInput value={input} onChangeText={e => setInput(e)} />
+          </View>
+        )}
+        {mode === 'Type' && (
+          <>
+            <Text>Type</Text>
+            {['Location', 'Quiz', 'Media']
+              .filter(type => {
+                console.log(
+                  type,
+                  filterDisplay.type.find(val => val === type),
+                );
+                return (
+                  filterDisplay.type.find(val => val === type) === undefined
+                );
+              })
+              .map(type => {
+                return (
+                  <>
+                    <Pressable onPress={() => setInput(type)}>
+                      <View>
+                        <Text
+                          style={
+                            input == type
+                              ? { fontWeight: 'bold' }
+                              : { fontWeight: 'normal' }
+                          }>
+                          {type}
+                        </Text>
+                      </View>
+                    </Pressable>
+                  </>
+                );
+              })}
+          </>
+        )}
+        {mode === 'Distance' && (
+          <>
+            <Text>Maximum Acceptable Distance</Text>
+            <TextInput
+              inputMode="numeric"
+              value={input}
+              onChangeText={e => setInput(e)}
+            />
+          </>
+        )}
+        {mode === 'Difficulty' && (
+          <>
+            <Text> Target Average Difficulty</Text>
+            <TextInput
+              inputMode="numeric"
+              value={input}
+              onChangeText={e => setInput(e)}
+            />
+          </>
+        )}
+        {mode === 'Rating' && (
+          <>
+            <Text>Minimum Rating</Text>
+            <TextInput
+              inputMode="numeric"
+              value={input}
+              onChangeText={e => setInput(e)}
+            />
+          </>
+        )}
+        {mode === 'Text' && (
+          <>
+            <Text>Text Includes</Text>
+            <TextInput value={input} onChangeText={e => setInput(e)} />
+          </>
+        )}
+        {mode != 'empty' && (
+          <View>
+            <Button title="Add Filter" onPress={addFilter} />
+          </View>
+        )}
+      </View>
+      <View>
+        <Text> Where We'll Store parts that have been added.</Text>
+        {filterDisplay.tags.map(tag => (
+          <Text key={tag}>Tag:{tag}</Text>
+        ))}
+
+        {filterDisplay.textIncludes.map(text => (
+          <Text key={text}>Text:{text}</Text>
+        ))}
+
+        {filterDisplay.type.map(type => (
+          <Text key={type}>Type:{type}</Text>
+        ))}
+
+        {filterDisplay.maxDistance !== 0 && (
+          <Text>Max Distance:{filterDisplay.maxDistance}</Text>
+        )}
+
+        {filterDisplay.minimumAverageRating !== 0 && (
+          <Text>Minimum Rating:{filterDisplay.minimumAverageRating}</Text>
+        )}
+        {filterDisplay.targetDifficulty !== 0 && (
+          <Text>Target Difficulty:{filterDisplay.targetDifficulty}</Text>
+        )}
+      </View>
+      <View>
+        <Text> Where We'll Store Results of a Search</Text>
+      </View>
+    </View>
   );
 };
-export default SearchBar;
+export default Search;
